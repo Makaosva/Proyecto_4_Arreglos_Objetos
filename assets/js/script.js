a5b7f930234678d67e1ede5d0aeb003067fd4096
Makaosva <maka.osses@gmail.com>
2024-04-20 01:48:22 -0400

    feat/Agregar titulo principal

---
+++ assets/js/script.js
@@ -0,0 +1,95 @@
+export const propiedades_alquiler = [
+  {
+    nombre: "Alquiler1",
+    src: "Cabaña acogedora en la montaña",
+    descripcion:
+      "Esta cabaña de 3 habitaciones esta ubicado en un bosque en la montaña, para escapar de ruido",
+    ubicacion: "456 Ausable Dr, Vermont, VE 76325",
+    habitaciones: 0,
+    costo: 0,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: "Alquiler2",
+    src: "Apartamento en el centro de la ciudad",
+    descripcion:
+      "Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.",
+    ubicacion: "123 Main Street, Anytown, CA 91234",
+    habitaciones: 2,
+    costo: 2000,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: "Alquiler3",
+    src: "Apartamento luminoso con vista al mar",
+    descripcion:
+      "Este hermoso apartamento ofrece una vista impresionante al mar",
+    ubicacion: "456 Ocean Avenue, Seaside Town, CA 56789",
+    habitaciones: 3,
+    costo: 2500,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Alquiler4",
+    src: "Condominio moderno en zona residencial",
+    descripcion:
+      "Este elegante condominio moderno está ubicado en una tranquila zona residencial",
+    ubicacion: "123 Main Street, Anytown, CA 91234",
+    habitaciones: 2,
+    costo: 2200,
+    smoke: false,
+    pets: false,
+  },
+];
+//export default Propiedades alquiler
+
+export const propiedades_venta = [
+  {
+    nombre: "Venta1",
+    src: "Penthouse de lujo con terraza panorámica",
+    descripcion:
+      "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+    ubicacion: "567 Skyline Avenue, Skyview City, CA 56789",
+    habitaciones: 3,
+    costo: 4700,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: "Venta2",
+    src: "Apartamento acogedor en la montaña",
+    descripcion:
+      "Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas",
+    ubicacion: "789 Mountain Road, Summit Peaks, CA 23456",
+    habitaciones: 2,
+    costo: 1600,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Venta3",
+    src: "Apartamento de lujo en zona exclusiva",
+    descripcion:
+      "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+    ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
+    habitaciones: 4,
+    costo: 5500,
+    smoke: false,
+    pets: false,
+  },
+  {
+    nombre: "Venta4",
+    src: "Apartamento acogedor en la playa",
+    descripcion:
+      "Este apartamento acogedor está situado al frente de la playa con impresionantes vistas",
+    ubicacion: "2001 S Surf Rd, Hollywood, FL 33019",
+    habitaciones: 3,
+    costo: 4600,
+    smoke: false,
+    pets: false,
+  },
+];
+//export default Propiedades venta
